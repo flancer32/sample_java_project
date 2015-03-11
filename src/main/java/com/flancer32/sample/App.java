4ed39daf0703591721ff28c33364028205807ff6
Alex Gusev <flancer64@gmail.com>
2015-03-11 12:27:15 +0200

    OWN-36: logging

--- src/main/java/com/flancer32/sample/App.java
+++ src/main/java/com/flancer32/sample/App.java
@@ -6,6 +6,9 @@ package com.flancer32.sample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
 public class App {
 	final static Logger log = LoggerFactory.getLogger(App.class);
 
@@ -14,6 +17,9 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		log.info("Start");
+		log.info("=== Logger internal state ===");
+		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+		StatusPrinter.print(lc);
 		log.info("Stop");
 	}
 
